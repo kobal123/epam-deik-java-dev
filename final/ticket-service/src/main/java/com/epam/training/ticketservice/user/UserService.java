7a0eb0a0cdc648be4ac330e839c35427a70ca6a3
Kósa Balázs <emberbencsface@gmail.com>
2022-10-11 23:03:46 +0200

    Changed repository to JPARepository, Refactored UserService.java

--- final/ticket-service/src/main/java/com/epam/training/ticketservice/user/UserService.java
+++ final/ticket-service/src/main/java/com/epam/training/ticketservice/user/UserService.java
@@ -2,18 +2,42 @@ package com.epam.training.ticketservice.user;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
-    private User currentUser;
+    private Optional<User> currentUser = Optional.empty();
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
 
-    public boolean currentUserHasAuthority(Role role){
-        return currentUser.getRoles().contains(role);
+    public boolean signInUser(String userName, String password) {
+        User user = userRepository.findById(userName).orElseThrow(()-> new BadCredentialsException("No such username"));
+
+        if(!password.equals(user.getPassword())){
+            return false;
+        }
+        currentUser = Optional.of(user);
+        return true;
+    }
+
+    public void signOutUser() {
+        currentUser = Optional.empty();
+    }
+
+    public Optional<User> getCurrentUser() {
+        return currentUser;
+    }
+
+    public boolean isUserLoggedIn() {
+        return currentUser.isPresent();
+    }
+
+    private boolean currentUserHasAuthority(Role role) {
+        return currentUser.isPresent() && currentUser.get().getRoles().contains(role);
     }
 }
