0966e045cd1b092c8091d4d9dd4c55015345f18d
Kósa Balázs <emberbencsface@gmail.com>
2022-10-11 22:58:46 +0200

    Create admin user on startup

--- final/ticket-service/src/main/java/com/epam/training/ticketservice/Application.java
+++ final/ticket-service/src/main/java/com/epam/training/ticketservice/Application.java
@@ -1,12 +1,21 @@
 package com.epam.training.ticketservice;
 
-import org.springframework.boot.Banner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
+import com.epam.training.ticketservice.user.Role;
+import com.epam.training.ticketservice.user.User;
+import com.epam.training.ticketservice.user.UserRepository;
+import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import java.util.Set;
 
 @SpringBootApplication
 public class Application {
+
+
     public static void main(String[] args) {
         System.out.println("Hello world");
         SpringApplication app = new SpringApplication(Application.class);
@@ -14,5 +23,31 @@ public class Application {
         app.setBannerMode(Banner.Mode.OFF);
 
         app.run(args);
+
+
+    }
+
+
+
+    @Component
+    public class DataLoader implements CommandLineRunner {
+
+        private UserRepository userRepository;
+
+
+        public DataLoader(UserRepository userRepository) {
+            this.userRepository = userRepository;
+
+        }
+
+        @Override
+        public void run(String... args) throws Exception  {
+
+        }
+
+        @EventListener(ContextRefreshedEvent.class)
+        public void doSomethingAfterStartup() {
+            userRepository.save(new User("admin","admin", Set.of(Role.ADMIN,Role.USER)));
+        }
     }
 }
