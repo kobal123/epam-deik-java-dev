54a7cfe6f1118fae79cb09122a9088a1a17ebec8
Kósa Balázs <emberbencsface@gmail.com>
2022-10-14 00:41:28 +0200

    Added ScreeningId.java

---
+++ final/ticket-service/src/main/java/com/epam/training/ticketservice/screening/ScreeningId.java
@@ -0,0 +1,53 @@
+package com.epam.training.ticketservice.screening;
+
+import javax.persistence.Embeddable;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class ScreeningId  implements Serializable {
+
+    private String movieTitle;
+
+    private String roomName;
+
+    private LocalDateTime startTime;
+
+    public ScreeningId(String movieTitle, String roomName, LocalDateTime startTime) {
+        this.movieTitle = movieTitle;
+        this.roomName = roomName;
+        this.startTime = startTime;
+    }
+
+    public ScreeningId() {
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ScreeningId that = (ScreeningId) o;
+        return Objects.equals(movieTitle, that.movieTitle) && Objects.equals(roomName, that.roomName) && Objects.equals(startTime, that.startTime);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(movieTitle, roomName, startTime);
+    }
+
+    public String getMovieTitle() {
+        return movieTitle;
+    }
+
+    public String getRoomName() {
+        return roomName;
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+}
