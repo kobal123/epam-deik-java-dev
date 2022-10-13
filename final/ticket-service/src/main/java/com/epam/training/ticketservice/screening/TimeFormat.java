b7ce224a663c814faadb8adaa1e97ccccca67135
Kósa Balázs <emberbencsface@gmail.com>
2022-10-14 00:43:09 +0200

    Added TimeFormat.java

---
+++ final/ticket-service/src/main/java/com/epam/training/ticketservice/screening/TimeFormat.java
@@ -0,0 +1,15 @@
+package com.epam.training.ticketservice.screening;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import java.time.format.DateTimeFormatter;
+
+@Component
+public class TimeFormat {
+
+    @Bean
+    public DateTimeFormatter formatter(){
+        return DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+    }
+}
